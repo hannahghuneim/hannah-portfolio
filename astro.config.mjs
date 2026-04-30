bc3a76578634c4c3981a5a03617fa89a3f6f5206
Hannah Ghuneim <hghuneim@depaul.edu>
2026-04-30 12:09:45 -0500

    troubleshooting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://hannahghuneim.github.io/hannah-portfolio/',
-  base: '/hannah-portfolio',
-})
+  output: "static",
+  base: "/hannah-portfolio",
+});
